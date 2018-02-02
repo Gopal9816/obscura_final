27c30b0a1e37ba2f0df10c4cab576e9f5dd798f7
RHariSankar <rhari.s7@gmail.com>
2018-02-02 23:15:54 +0530

    padam maati

--- resources/views/levels/jabberwocky.blade.php
+++ resources/views/levels/jabberwocky.blade.php
@@ -16,19 +16,8 @@
     <div class="col-md-1 col-xs-1"></div>
 </div>
 <div class="row" >
-    <div class="col-md-6 col-xs-6 hand" align="center">
-        <a href="/game/red">
-            <!--<img src="{{ asset('img/p1.png') }}" class="img-responsive">-->
-            
-            <img src="../img/jw1.jpg" class="img-responsive">
-        </a> 
-    </div>
-    <div class="col-md-6 col-xs-6 hand" align="center">
-        <a href="/game/blue">
-            <!--<img src="{{ asset('img/p2.png') }}" class="img-responsive" >-->
-           
-            <img src="../img/jw2.jpg" class="img-responsive">
-        </a> 
+    <div class="col-md-6 col-xs-12 col-md-offset-3  hand" align="center">
+       <img src="../img/jwy.png" class="img-responsive">
     </div>
 
 <div class="col-md-6 col-md-offset-3" style="padding-bottom: 20px;">        
