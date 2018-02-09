e4e3b768f584ff7a744ae065f95693aa2e55c9b2
RHariSankar <rhari.s7@gmail.com>
2018-02-09 22:38:18 +0530

    register page modified

--- resources/views/auth/registerNew.blade.php
+++ resources/views/auth/registerNew.blade.php
@@ -1,20 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<h1>Register New</h1>
+<style>
+  
+</style>
+<h1 id="heading">Register New</h1>
 <div class="row">
     <div class="col-md-6 col-md-offset-3">
         <div class="panel panel-default">
             <div class="panel-body">
+        
+
         {!! Form::open(['route' => ['register',$id], 'method' => 'POST']) !!}
             <div class="form-group">
                 {{Form::label('phno','Phone Number')}}
-                {{Form::text('phno','',['class' => 'form-control','placeholder' => 'Phone Number'])}}
+                {{Form::text('phno','',['class' => 'form-control','placeholder' => 'Phone Number','required'])}}
             </div>
            
             <div class="form-group">
-                {{Form::radio('college','home',false,['class' => 'form-control', 'onchange' => 'hideother()'])}}Home <br />
-                {{Form::radio('college','others',false,['class' => 'form-control','id' => 'other' , 'onchange' => 'displayother()'])}}Others
+                
+                {{Form::radio('college','home',false, ['id' => 'home' , 'onchange' => 'hideother()','required'])}}Home <br>
+                {{Form::radio('college','others',false, ['id' => 'other' , 'onchange' => 'displayother()','required'])}}Others
+            </div>
+            <div class="form-group">
+                {{Form::label('collegename','College Name')}}
                 {{Form::text('collegename','',['class' => 'form-control','placeholder' => 'College', 'id' => 'inputother'])}}
             </div>
             
--- storage/framework/views/3034da85bcab802a228cdbdb7317f29aab73562a.php
+++ storage/framework/views/3034da85bcab802a228cdbdb7317f29aab73562a.php
@@ -1,21 +1,31 @@
 <?php $__env->startSection('content'); ?>
-<h1>Register New</h1>
+<style>
+  
+</style>
+<h1 id="heading">Register New</h1>
 <div class="row">
     <div class="col-md-6 col-md-offset-3">
         <div class="panel panel-default">
             <div class="panel-body">
+        
+
         <?php echo Form::open(['route' => ['register',$id], 'method' => 'POST']); ?>
 
             <div class="form-group">
                 <?php echo e(Form::label('phno','Phone Number')); ?>
 
-                <?php echo e(Form::text('phno','',['class' => 'form-control','placeholder' => 'Phone Number'])); ?>
+                <?php echo e(Form::text('phno','',['class' => 'form-control','placeholder' => 'Phone Number','required'])); ?>
 
             </div>
            
             <div class="form-group">
-                <?php echo e(Form::radio('college','home',false,['class' => 'form-control', 'onchange' => 'hideother()'])); ?>Home <br />
-                <?php echo e(Form::radio('college','others',false,['class' => 'form-control','id' => 'other' , 'onchange' => 'displayother()'])); ?>Others
+                
+                <?php echo e(Form::radio('college','home',false, ['id' => 'home' , 'onchange' => 'hideother()','required'])); ?>Home <br>
+                <?php echo e(Form::radio('college','others',false, ['id' => 'other' , 'onchange' => 'displayother()','required'])); ?>Others
+            </div>
+            <div class="form-group">
+                <?php echo e(Form::label('collegename','College Name')); ?>
+
                 <?php echo e(Form::text('collegename','',['class' => 'form-control','placeholder' => 'College', 'id' => 'inputother'])); ?>
 
             </div>
